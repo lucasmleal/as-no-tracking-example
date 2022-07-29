dea7bef19e0b1872e23862526f83130933a8f6c8
Lucas Leal <lucas.leal@vigil.global>
2022-07-29 10:35:10 -0300

    initial commit

---
+++ AsNoTrackingTest/Context.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsNoTrackingTest;
+
+public class Context : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=playground;Username=postgres");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
---
+++ AsNoTrackingTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using AsNoTrackingTest;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+
+var gc0 = GC.CollectionCount(0);
+var gc1 = GC.CollectionCount(1);
+var gc2 = GC.CollectionCount(2);
+
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+Parallel.For(0, 1000, index =>
+{
+    using var db = new Context();
+
+    var list1 = db.Blogs
+        .AsNoTracking()
+        .ToList();
+
+    var list2 = db.Posts
+        .AsNoTracking()
+        .ToList();
+});
+
+stopWatch.Stop();
+
+Console.WriteLine($"Time: {stopWatch.ElapsedMilliseconds} ms");
+Console.WriteLine($"GC0: {GC.CollectionCount(0) - gc0}");
+Console.WriteLine($"GC1: {GC.CollectionCount(1) - gc1}");
+Console.WriteLine($"GC2: {GC.CollectionCount(2) - gc2}");
+
+Console.Read();
+
+
+
+/* for (int i = 0; i < 1000; i++)
+{
+    db.Add(new Blog { Url = "http://blogs.msdn.com/adonet", Posts = new List<Post> { new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" } } });
+    db.SaveChanges();
+
+    var blog = db.Blogs
+        .OrderBy(b => b.BlogId)
+        .Last();
+
+    Console.WriteLine("Updating the blog and adding a post");
+    for (int j = 0; j < 10; j++)
+    {
+        blog.Posts.Add(
+        new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+        db.SaveChanges();
+    }
+}*/
+
